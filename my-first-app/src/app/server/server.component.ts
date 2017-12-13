6cc1b46d9164641fbbe797c47311e418780728af
arsalan <arsalan@lightful.com>
2017-12-13 17:43:43 +0000

    directives

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -3,14 +3,39 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
+    // we can add style here too
+    // styles: [`
+    //     .online {
+    //         color: white;
+    //     }
+    // `]
     
 })
 
 export class ServerComponent {
     serverId=  10;
+    serverName = "";
+    allowCreateServer = false;
+    displayServer =  false;
     serverStatus=  'Offline';
 
-    getServerStatus() {
-        return this.serverStatus;
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+    }
+    
+    // Crete server on click
+    createServer() {
+        this.displayServer =  true;
+    }
+
+    // Show server name
+    serverStatusText() {
+        this.displayServer = false;
+    }
+
+    //Change color for online/offline server
+    getColor() {
+        return this.serverStatus === 'Online' ? 'green' : 'red';
     }
 }
