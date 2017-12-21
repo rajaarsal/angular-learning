e0865bb4f6d7075ee1418b6389d229a725cc11f4
arsalan <arsalan@lightful.com>
2017-12-21 11:34:20 +0000

    added shoping list and recipe list components

--- cooking-book/src/app/app.module.ts
+++ cooking-book/src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { RecepieListComponent } from './recepies/recepie-list/recepie-list.component';
+import { RecepieDetailComponent } from './recepies/recepie-detail/recepie-detail.component';
+import { RecepieItemComponent } from './recepies/recepie-list/recepie-item/recepie-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    RecepiesComponent,
+    RecepieListComponent,
+    RecepieDetailComponent,
+    RecepieItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
